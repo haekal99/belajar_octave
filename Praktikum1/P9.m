3b6ebc35318ec10c9585041766d9c314bb211a7d
Muhammad Haekal <56957271+haekal99@users.noreply.github.com>
2022-01-03 18:05:30 +0800

    Add files via upload

---
+++ Praktikum1/P9.m
@@ -0,0 +1,24 @@
+F=imread('C:\Users\ACER\Documents\Praktek Program TI\Pengolahan Citra\Praktikum1\citra.jpg');
+[tinggi, lebar]=size(F);
+sx=45;% penggeseran arah horizontal
+sy=-35;% penggeseran arah vertikal
+F2=double(F);
+G=zeros (size(F2));
+for y=1 : tinggi
+  for x=1 :lebar
+    xlama = x - sx;
+    ylama = y - sy;
+    
+    if (xlama>=1)&& (xlama<=lebar) &&...
+      (ylama>=1)&& (ylama<=tinggi)
+      G(y,x)= F2(ylama,xlama);
+    else
+      G(y,x)=0;
+    end
+  end
+end
+G=uint8(G);
+figure(1); imshow(G);
+imwrite (G,'C:\Users\ACER\Documents\Praktek Program TI\Pengolahan Citra\Praktikum1\hasilp9.jpg');
+
+clear all;
---
+++ Praktikum1/per92.m
@@ -0,0 +1,26 @@
+F=imread ('C:\Users\ACER\Documents\Praktek Program TI\Pengolahan Citra\Praktikum1\aku.jpg');
+[tinggi, lebar]=size(F);
+
+sudut =10% sudut pemutaran
+rad = pi*sudut /150;
+cosa=cos(rad);
+sina=sin(rad);
+F2=double(F);
+for y=1 : tinggi
+  for x=1 : lebar
+    x2 = round(x * cosa + y * sina);
+    y2 = round(y * cosa - x * sina);
+    
+    if (x2>=1) && (x2<=lebar) &&...
+      (y2>=1) && (y2<=tinggi)
+      G(y,x) - F2 (y2,x2);
+    else
+      G(y,x)=0;
+      
+    end
+  end
+end
+G=uint8(G);
+figure(1);imshow(G);
+imwrite(G,'C:\Users\ACER\Documents\Praktek Program TI\Pengolahan Citra\hasil92');
+clear all;
